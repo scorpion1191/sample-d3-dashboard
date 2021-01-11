5cafd5090a5dc1e04beb8a39144848e8f18fd84e
subbu <subbu1191@gmail.com>
2021-01-11 21:24:22 +0530

    added gitingnore
    added gitingnore

--- public.js
+++ public.js
@@ -1,24 +1,13 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('bodyParser');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(bodyParser.json());
-        //support application/x-www-form-urlencoded post data
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(function(req, res, next) {
-            res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, x-access-token, Content-Type, Accept");
-            next();
-          });
+var express = require('express');
+var path = require('path');
+var app = express();
+var port = process.env.PORT || 3000;
         
 app.use(express.static(path.join(__dirname, "public")))
 
-        app.get("*", (req, res) => {
-            res.sendFile(path.join(__dirname, "public","index.html"));
-        });
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public","index.html"));
+});
 
 app.listen(port, function () {
             console.log('Example app listening on port 3000!');
