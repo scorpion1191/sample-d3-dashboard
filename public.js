955e24e7f7ea68439b448f390dbb86419f2bdcee
subbu <subbu1191@gmail.com>
2021-01-11 20:54:57 +0530

    changed server
    changed server

--- public.js
+++ public.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get('*', (req, res) => {
-    res.sendFile('./public/index.html', { root: __dirname });
-});
+app.use(bodyParser.json());
+        //support application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(port, () => console.log(`listening on port ${port}!`))
+app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, x-access-token, Content-Type, Accept");
+            next();
+          });
+        
+app.use(express.static(path.join(__dirname, "public")))
+
+        app.get("*", (req, res) => {
+            res.sendFile(path.join(__dirname, "public","index.html"));
+        });
+
+app.listen(port, function () {
+            console.log('Example app listening on port 3000!');
+        });
